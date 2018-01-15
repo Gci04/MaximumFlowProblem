f7e6c01ac6e8cad4e37a024b9c9cf0bc62482f34
Gci04 <gcinzoe04@gmail.com>
2018-01-15 11:46:36 +0300

    Update PushRelabel.h

--- MaximumFlowProblem/PushRelabel.h
+++ MaximumFlowProblem/PushRelabel.h
@@ -3,7 +3,7 @@
 #include "Network.h"
 #include <queue> 
 
-void Push(Vertex* u, Vertex* v, Edge * uv);
+void Push(Vertex* u, Vertex* v , Edge * uv);
 void Init_Preflow(Network *G);
 void GenericPushRelabel(Network * G);
 void PushRelabel(Network * G);
@@ -12,12 +12,4 @@ bool TryPush(Vertex* u);
 bool TryRelabel(Vertex * u);
 void Relabel(Vertex * u);
 bool overFlowingVertex(std::vector<Vertex*> vertices);
-<<<<<<< HEAD
-<<<<<<< HEAD
-void UpdateOriginalNetwork(Network * Original, std::vector<Vertex *> res_vertices);
-=======
 void UpdateOriginalNetwork(Network * Original , std::vector<Vertex *> res_vertices);
->>>>>>> bb0610d2d4e3583c6edb919174e69388d03c1d52
-=======
-void UpdateOriginalNetwork(Network * Original , std::vector<Vertex *> res_vertices);
->>>>>>> bb0610d2d4e3583c6edb919174e69388d03c1d52
